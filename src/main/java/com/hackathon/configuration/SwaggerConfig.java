3088bdd49ef43285c44aef788833eac187f05c61
Fordclifford <cliffordmasi07@gmail.com>
2019-06-12 13:56:01 +0300

    final

--- src/main/java/com/hackathon/configuration/SwaggerConfig.java
+++ src/main/java/com/hackathon/configuration/SwaggerConfig.java
@@ -1,16 +1,11 @@
 package com.hackathon.configuration;
 
-import java.util.ArrayList;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ParameterBuilder;
-import springfox.documentation.builders.PathSelectors;
 import static springfox.documentation.builders.PathSelectors.regex;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelReference;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -24,25 +19,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
      @Bean
     public Docket productApi() {
-                       ParameterBuilder aParameterBuilder = new ParameterBuilder();
-        aParameterBuilder.name("Authorization")                 // name of header
-                         .modelRef((ModelReference) new ModelRef("string"))
-                         .parameterType("header")               // type - header
-                         .defaultValue("Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNTYwMzMxNTg1LCJleHAiOjE1NjA5MzYzODV9.jFk7O6ron1ed5ZSqSZd2FArHU2x3gRsQOfwaQA53sl8kBhFiyIBZCecG3nJ1DzQxwkIbHZXEAO_hGfbQCKqq0g")        // based64 of - zone:mypassword
-                         .required(true)                // for compulsory
-                         .build();
-        java.util.List<Parameter> aParameters = new ArrayList<>();
-        aParameters.add(aParameterBuilder.build());             // add parameter
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                                                      .apis(RequestHandlerSelectors
-                                                              .any())
-                 .apis(RequestHandlerSelectors.basePackage("com.hackathon"))
-                                                      .paths(PathSelectors.any())
-                                                      .build().
-                                                              pathMapping("/api.*")
-                                                      .globalOperationParameters(aParameters);
-               
-             
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.hackathon"))
+                .paths(regex("/api.*"))
+                .build()
+                .apiInfo(metaInfo());
     }
 
     private ApiInfo metaInfo() {
